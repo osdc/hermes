9ce306ac16b01decaeec54a19d08d4f9a6833209
Avijit Gupta <526avijit@gmail.com>
2016-10-04 20:16:21 +0530

    Initial react setup with router, es6, babel, webpack

---
+++ app/index.js
@@ -0,0 +1,36 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { AppContainer } from 'react-hot-loader';
+import configureStore from './store/configureStore';
+import Root from './containers/Root';
+import './styles/main.scss';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
+
+const target = document.getElementById('root');
+const node = (
+    <AppContainer>
+      <Root store={store} history={history} />
+    </AppContainer>);
+render(
+  node,
+  target
+);
+
+if (module.hot) {
+  module.hot.accept('./containers/Root', () => {
+    const NewRoot = require('./containers/Root').default;
+    const newNode = (
+        <AppContainer>
+          <NewRoot store={store} history={history} />
+        </AppContainer>);
+    render(
+      newNode,
+      target
+    );
+  });
+}
---
+++ app/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+
+export default (
+	<Route path="/" component={App}>
+	</Route>
+);
