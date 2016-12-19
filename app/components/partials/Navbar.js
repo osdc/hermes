2409240634f5eb653b2357862028af576310502a
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-19 23:04:10 +0530

    Design fixes

--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Navbar = () =>
-    <nav>
+    <div className="navbar">
         <span className="logo">Hermes</span>
         <ul className="nav-left visible-lg">
             <li><a href="#">Dashboard</a></li>
@@ -11,9 +11,10 @@ const Navbar = () =>
         <ul className="nav-right">
             <li><a href="#"><span className="focus">8</span></a></li>
             <li><a href="#">Preferences</a></li>
-            <li><a href="#">Logout (Ujjwal Sharma)</a></li>
+            <li><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
         </ul>
-    </nav>;
+        
+    </div>;
 
 
 export default Navbar;
