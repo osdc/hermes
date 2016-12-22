471d9f30b607f4e47f2ef1813a4ba5b444f1219c
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-22 10:38:12 +0530

    Add Footer content

--- app/components/partials/Footer.js
+++ app/components/partials/Footer.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Footer = () =>
-    <div className="footer">
-        <div className="container">Footer</div>
+    <div className="footer bar">
+        <ul>
+            <li><a href="https://github.com/osdc/hermes/tags">Changelog</a></li>
+            <li><a href="https://github.com/osdc/hermes">Github</a></li>
+            <li><a href="https://raw.githubusercontent.com/osdc/hermes/master/AUTHORS">{ /* TODO: Add a modal-like display of contributors rather than redirecting to Github */ }Contributors</a></li>
+            <li>License: GPL-3.0</li>
+        </ul>
+        <span className="bar-right">Made with ❤ by <a href="https://github.com/osdc">OSDC</a></span>
     </div>;
 
 
