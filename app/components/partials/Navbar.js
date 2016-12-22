b090c1412601174085c3e3ef9055479d0ca4228b
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-22 10:38:12 +0530

    Add Footer content

--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Navbar = () =>
-    <div className="navbar">
+    <div className="navbar bar">
         <span className="logo"><a href="#">Hermes</a></span>
-        <ul className="nav-left visible-lg">
+        <ul className="visible-lg">
             <li><a href="#">Dashboard</a></li>
             <li><a href="#">Hubs</a></li>
             <li><a href="#">Events</a></li>
         </ul>
-        <ul className="nav-right">
+        <ul className="bar-right">
             <li><a href="#"><span className="focus">8</span></a></li>
             <li><a href="#">Preferences</a></li>
             <li><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
