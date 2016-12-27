415eae545dd169f94d9f2627d76abec2e4bcb654
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-27 21:48:11 +0530

    Fix inconsistencies

--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const Navbar = () =>
-    <div className="navbar bar">
-        <span className="logo"><a href="#">Hermes</a></span>
-        <ul className="visible-lg">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Hubs</a></li>
-            <li><a href="#">Events</a></li>
-        </ul>
-        <ul className="bar-right">
-            <li><a href="#"><span className="focus">8</span></a></li>
-            <li><a href="#">Preferences</a></li>
-            <li><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
-        </ul>
-        
-    </div>;
+	<div className="navbar bar">
+		<span className="logo"><a href="#">Hermes</a></span>
+		<ul className="visible-lg">
+			<li><a href="#">Dashboard</a></li>
+			<li><a href="#">Hubs</a></li>
+			<li><a href="#">Events</a></li>
+		</ul>
+		<ul className="bar-right">
+			<li><a href="#"><span className="focus">8</span></a></li>
+			<li><a href="#">Preferences</a></li>
+			<li><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
+		</ul>
+		
+	</div>;
 
 
 export default Navbar;
