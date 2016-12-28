b2024568cf16fec3ff2711d5c5de049042cf179a
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-28 17:29:18 +0530

    Fix indentation

--- app/components/partials/Footer.js
+++ app/components/partials/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () =>
 	<div className="footer bar">
 		<ul>
-			<li><a href="https://github.com/osdc/hermes/tags">Changelog</a></li>
-			<li><a href="https://github.com/osdc/hermes">Github</a></li>
-			<li><a href="https://raw.githubusercontent.com/osdc/hermes/master/AUTHORS">{ /* TODO: Add a modal-like display of contributors rather than redirecting to Github */ }Contributors</a></li>
-			<li>License: GPL-3.0</li>
+			<li className="list-item"><a href="https://github.com/osdc/hermes/tags">Changelog</a></li>
+			<li className="list-item"><a href="https://github.com/osdc/hermes">Github</a></li>
+			<li className="list-item"><a href="https://raw.githubusercontent.com/osdc/hermes/master/AUTHORS">{ /* TODO: Add a modal-like display of contributors rather than redirecting to Github */ }Contributors</a></li>
+			<li className="list-item">License: GPL-3.0</li>
 		</ul>
 		<span className="bar-right">Made with ❤ by <a href="https://github.com/osdc">OSDC</a></span>
 	</div>;
--- app/components/partials/Header.js
+++ app/components/partials/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Header = () =>
-    <div className="header">
-        <h1 className="text-center text-primary">Hermes</h1>
-    </div>;
+  <div className="header">
+    <h1 className="text-center text-primary">Hermes</h1>
+  </div>;
 
 
 export default Header;
