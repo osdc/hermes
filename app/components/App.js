f85f558274997a637ed86a79bc341c7390174cfe
Ujjwal Sharma <usharma1998@gmail.com>
2016-12-28 17:29:18 +0530

    Fix indentation

--- app/components/App.js
+++ app/components/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const About = () =>
-	<div>
-		<div className="main">
-			<h2 className="text-primary text-center">Upcoming Events</h2>
-			<div className="panel">
-				{ /* TODO: Replace hardcoded data with data from the backend */ }
-				<h3 className="panel-heading">
-					<span className="focus">HUB</span> Title of Event
-				</h3>
-				<div className="panel-body">
-					<p>A small description of the event.</p>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu aliquet felis. Fusce consectetur suscipit ipsum, eu convallis diam cursus vel. Praesent molestie dui quis ultricies pharetra. Pellentesque hendrerit sed massa quis accumsan. Mauris odio mi, aliquam at metus in, bibendum consectetur augue. Duis finibus, massa at pellentesque gravida, ante velit hendrerit enim, quis facilisis sapien velit ut justo. Fusce ut velit vel sem porttitor rutrum in a mauris. Etiam tristique leo quis odio feugiat condimentum. Cras in nunc consectetur, malesuada eros at, accumsan lacus.</p>
-				</div>
-			</div>
-		</div>
-		<div className="sidebar">
-			<h2 className="text-primary text-center">Trending</h2>
-		</div>
-	</div>;
+  <div>
+    <div className="main">
+      <h2 className="text-primary text-center">Upcoming Events</h2>
+      <div className="panel">
+        { /* TODO: Replace hardcoded data with data from the backend */ }
+        <h3 className="panel-heading">
+          <span className="focus">HUB</span> Title of Event
+        </h3>
+        <div className="panel-body">
+          <p>A small description of the event.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu aliquet felis. Fusce consectetur suscipit ipsum, eu convallis diam cursus vel. Praesent molestie dui quis ultricies pharetra. Pellentesque hendrerit sed massa quis accumsan. Mauris odio mi, aliquam at metus in, bibendum consectetur augue. Duis finibus, massa at pellentesque gravida, ante velit hendrerit enim, quis facilisis sapien velit ut justo. Fusce ut velit vel sem porttitor rutrum in a mauris. Etiam tristique leo quis odio feugiat condimentum. Cras in nunc consectetur, malesuada eros at, accumsan lacus.</p>
+        </div>
+      </div>
+    </div>
+    <div className="sidebar">
+      <h2 className="text-primary text-center">Trending</h2>
+    </div>
+  </div>;
 
 
 export default About;
--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -4,16 +4,15 @@ const Navbar = () =>
 	<div className="navbar bar">
 		<span className="logo"><a href="#">Hermes</a></span>
 		<ul className="visible-lg">
-			<li><a href="#">Dashboard</a></li>
-			<li><a href="#">Hubs</a></li>
-			<li><a href="#">Events</a></li>
+			<li className="list-item"><a href="#">Dashboard</a></li>
+			<li className="list-item"><a href="#">Hubs</a></li>
+			<li className="list-item"><a href="#">Events</a></li>
 		</ul>
 		<ul className="bar-right">
-			<li><a href="#"><span className="focus">8</span></a></li>
-			<li><a href="#">Preferences</a></li>
-			<li><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
+			<li className="list-item"><a href="#"><span className="focus">8</span></a></li>
+			<li className="list-item"><a href="#">Preferences</a></li>
+			<li className="list-item"><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
 		</ul>
-		
 	</div>;
 
 
