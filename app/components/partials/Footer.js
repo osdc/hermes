5c3f6f6ebd6c82e87a8cadeff42558fca8a18971
Ujjwal Sharma <usharma1998@gmail.com>
2017-01-01 12:57:22 +0530

    Remove License text from Footer

--- app/components/partials/Footer.js
+++ app/components/partials/Footer.js
@@ -6,7 +6,6 @@ const Footer = () =>
       <li className="list-item"><a href="https://github.com/osdc/hermes/tags">Changelog</a></li>
       <li className="list-item"><a href="https://github.com/osdc/hermes">Github</a></li>
       <li className="list-item"><a href="https://raw.githubusercontent.com/osdc/hermes/master/AUTHORS">{ /* TODO: Add a modal-like display of contributors rather than redirecting to Github */ }Contributors</a></li>
-      <li className="list-item">License: GPL-3.0</li>
     </ul>
     <div className="spacer" />
     <div className="vertical-center text-center">
