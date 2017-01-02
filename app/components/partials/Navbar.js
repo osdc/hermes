09fa69dcd8b618a1a45be065806852bd587d8191
Ujjwal Sharma <usharma1998@gmail.com>
2017-01-02 22:53:05 +0530

    Minor fixes

--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const iconClose = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_close_white_48dp.png';
-const iconMenu = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png';
+const ICON_CLOSE = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_close_white_48dp.png';
+const ICON_MENU = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png';
 
 function toggleDropdown() {
-  var img = document.getElementById('dropdown-trigger');
-  img.src = (img.src == iconClose ? iconMenu : iconClose);
-  var x = document.getElementsByClassName('dropdown-content');
-  for (var i = 0; i < x.length; i++) {
-    x[i].classList.toggle('show');
-  }
+  let menuToggleIcon = document.getElementsByClassName('dropdown-trigger')[0];
+  menuToggleIcon.src = (menuToggleIcon.src === ICON_CLOSE ? ICON_MENU : ICON_CLOSE);
+  document.getElementsByClassName('dropdown-content')[0].classList.toggle('show');
 }
 
 const Navbar = () =>
   <div className="navbar bar">
     <div className="vertical-center logo text-center"><a href="#">Hermes</a></div>
-    <img onClick={toggleDropdown} id="dropdown-trigger" src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png" />
+    <img onClick={toggleDropdown}
+         className="dropdown-trigger"
+         src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png" />
     <div className="dropdown-content flex-row">
       <ul>
         <li className="list-item"><Link to="/">Dashboard</Link></li>
@@ -25,7 +24,7 @@ const Navbar = () =>
       </ul>
       <div className="spacer" />
       <ul>
-        <li className="list-item"><a href="#"><span className="focus">8</span></a></li>
+        <li className="list-item"><a href="#"><span className="focus">{ /* TODO: Remove Hardcoded Value*/ }8</span></a></li>
         <li className="list-item"><a href="#">Preferences</a></li>
         <li className="list-item"><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
       </ul>
