8074139476aa0c573cd9a79b6e48f9b54e9c1e76
Ujjwal Sharma <usharma1998@gmail.com>
2017-01-13 20:32:47 +0530

    Use SVGs locally and more SVG sorcery

--- app/components/partials/Navbar.js
+++ app/components/partials/Navbar.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const ICON_CLOSE = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_close_white_48dp.png';
-const ICON_MENU = 'https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png';
+const IconClose = require('babel!svg-react!../../../public/ic_close_white_48px.svg?name=IconClose');
+const IconMenu = require('babel!svg-react!../../../public/ic_menu_white_48px.svg?name=IconMenu');
 
-function toggleDropdown() {
-  let menuToggleIcon = document.getElementsByClassName('dropdown-trigger')[0];
-  menuToggleIcon.src = (menuToggleIcon.src === ICON_CLOSE ? ICON_MENU : ICON_CLOSE);
-  document.getElementsByClassName('dropdown-content')[0].classList.toggle('show');
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: false };
+  }
+
+  toggleDropdown() {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen
+    }));
+    document.getElementsByClassName('dropdown-content')[0].classList.toggle('show');
+  }
+
+  render() {
+    return (
+      <div className="navbar bar">
+        <div className="vertical-center logo text-center"><a href="#">Hermes</a></div>
+        {
+          this.state.isOpen ?
+          <IconClose className="dropdown-trigger" onClick={this.toggleDropdown.bind(this)} />
+          :
+          <IconMenu className="dropdown-trigger" onClick={this.toggleDropdown.bind(this)} />
+        }
+        <div className="dropdown-content flex-row">
+          <ul>
+            <li className="list-item"><Link to="/">Dashboard</Link></li>
+            <li className="list-item"><a href="#">Hubs</a></li>
+            <li className="list-item"><a href="#">Events</a></li>
+          </ul>
+          <div className="spacer" />
+          { /* Check if user is logged in */
+            true ?
+            <ul>
+              <li className="list-item"><Link to="/login">Login</Link></li>
+              <li className="list-item"><Link to="/register">Register</Link></li>
+            </ul>
+            :
+            <ul>
+              <li className="list-item"><a href="#"><span className="focus">{ /* TODO: Remove Hardcoded Value*/ }8</span></a></li>
+              <li className="list-item"><a href="#">Preferences</a></li>
+              <li className="list-item"><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
+            </ul>
+          }
+        </div>
+      </div>
+    );
+  }
 }
-
-const Navbar = () =>
-  <div className="navbar bar">
-    <div className="vertical-center logo text-center"><a href="#">Hermes</a></div>
-    <img onClick={toggleDropdown}
-         className="dropdown-trigger"
-         src="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/navigation/1x_web/ic_menu_white_48dp.png" />
-    <div className="dropdown-content flex-row">
-      <ul>
-        <li className="list-item"><Link to="/">Dashboard</Link></li>
-        <li className="list-item"><a href="#">Hubs</a></li>
-        <li className="list-item"><a href="#">Events</a></li>
-      </ul>
-      <div className="spacer" />
-      { /* Check if user is logged in */
-        true ?
-        <ul>
-          <li className="list-item"><Link to="/login">Login</Link></li>
-          <li className="list-item"><Link to="/register">Register</Link></li>
-        </ul>
-        :
-        <ul>
-          <li className="list-item"><a href="#"><span className="focus">{ /* TODO: Remove Hardcoded Value*/ }8</span></a></li>
-          <li className="list-item"><a href="#">Preferences</a></li>
-          <li className="list-item"><a href="#"> {/* TODO: Remove Harcoded Value */} Logout (Ujjwal Sharma)</a></li>
-        </ul>
-      }
-    </div>
-  </div>;
 
 export default Navbar;
